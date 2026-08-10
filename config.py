bb37b8c70545d1513511fd89d86b1a30f40fbd1e
diligence594-ship-it <diligence594@gmail.com>
2026-08-11 00:06:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = os.environ.get("API_HASH","511be78079ed5d4bd4c967bc7b5ee023")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Huuttrrtybot")
 BOT_TEXT = "@Txtextractorxxbot"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "7678862761"))
@@ -30,7 +30,7 @@ UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Txtextractorxxbot" #without @
+ADMIN_BOT_USERNAME = "Huuttrrtybot" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg")
 
@@ -38,14 +38,14 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfd
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "20567114"))
-# API_HASH = os.environ.get("API_HASH", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "AbhiTXT1bot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "8036182138"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8036182138").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441127578"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002402673518"))
+# API_ID = int(os.environ.get("API_ID", "22470912"))
+# API_HASH = os.environ.get("API_HASH", "511be78079ed5d4bd4c967bc7b5ee023")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Huuttrrtybot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "7678862761"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7678862761").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0")
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Demo23:Demo23@cluster0.fjar36c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "0"))
 # THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg")
 
