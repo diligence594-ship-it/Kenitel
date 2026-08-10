d39ece920393ef4644c3ca59c598c046825b0308
diligence594-ship-it <diligence594@gmail.com>
2026-08-10 23:25:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+from os import getenv
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "30929822"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","8586e9580c6480b65d23150cec959506")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8024972578:AAFahP8njNmQjN7Xywyo8BQ9Z_Q-232K7CI")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Txtextractorxxbot")
+BOT_TEXT = "@Txtextractorxxbot"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "8342248523"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003851031286"))
+
+# //FORCE_CHANNEL_ID
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003893979749"))
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003695294021"))
+# -----------------------------------------------
+join = '<a href="https://t.me/Anonymousextractor">✳️ JOIN BACKUP</a>'
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "Txtextractorxxbot" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg")
+
+
+
+
+# # Bot configuration
+# API_ID = int(os.environ.get("API_ID", "20567114"))
+# API_HASH = os.environ.get("API_HASH", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "AbhiTXT1bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "8036182138"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8036182138").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441127578"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002402673518"))
+# THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg")
+
